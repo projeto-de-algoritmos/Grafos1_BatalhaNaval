0bacfc7989874aace5cea34613918164ebba3cca
DaviPierre <zafirosafira@gmail.com>
2023-04-28 11:29:34 -0300

    Atualizacoes da logica do jogo e posicionamento dos navios

--- src/App.js
+++ src/App.js
@@ -2,22 +2,30 @@ import './App.css';
 
 function App() {
     const size = 10;
+    const tam = new Array(7);
     var campo = new Array(size);
     var coordl, coordc;
     var coord = [];
-    var tam = [];
     var orien = [];
     var aux;
     var k, i, j;
 
-    for (var i = 0; i < campo.length; i++) {
+    tam[0] = 1;
+    tam[1] = 1;
+    tam[2] = 2;
+    tam[3] = 2;
+    tam[4] = 3;
+    tam[5] = 4;
+    tam[6] = 5;
+
+    for (i = 0; i < campo.length; i++) {
         campo[i] = new Array(10).fill(0);
     }
 
     //gera número aleatório
     for (i = 0; i < 10; i++) {
 
-        while (k != 10) {
+        while (k !== 10) {
 
             aux = Math.floor(Math.random() * 100);
 
@@ -25,11 +33,11 @@ function App() {
 
             for (j = 0; j <= i; j++) {
 
-                if (aux == coord[j]) {
+                if (aux === coord[j]) {
                     k = 1;
                 }
 
-                else if (aux != coord[j] && k == 0) {
+                else if (aux !== coord[j] && k === 0) {
                     k = 10;
 
                 }
@@ -42,11 +50,6 @@ function App() {
         k = 0;
     }
 
-    for (i = 0; i < 10; i++) {
-        aux = Math.floor(Math.random() * 4) + 1;
-        tam[i] = aux;
-    }
-
     //Se 0 então vertical se 1 então horiontal
     for (i = 0; i < 10; i++) {
         aux = Math.floor(Math.random() * 100);
@@ -54,46 +57,30 @@ function App() {
     }
 
     //Posiciona os navios
-
+    k = 0;
     for (i = 0; i < 10; i++) {
-
         coordl = Math.floor(coord[i] / 10);
         coordc = coord[i] % 10;
-        campo[coordl][coordc] = 1;
+  
 
-        if (orien[i] == 1) {
-            k = 0;
-            for (j = 0; j < tam; j++) {
+        for (j = 0; j <= tam[i]; j++) {
 
-                if (coordc + j == 9) {
-
-                    campo[coordl][coordc - j - k] = 1;
-
-                }
-
-                else {
-                    campo[coordl][coordc + j] = 1;
+            //Navio na horizontal
+            if (orien[i] === 1) {
+                if (campo[coordl + j][coordc] === 0 && coordl + j <= 10) {
+                    campo[coordl + j][coordc] = 1;
                     k++;
                 }
 
             }
 
-        }
+            else if (orien[i] === 0) {
 
-        else {
-            k = 0;
-            for (j = 0; j < tam; j++) {
-
-                if (coordc + j == 9) {
-
-                    campo[coordl - j - k][coordc] = 1;
-
-                }
-
-                else {
-                    campo[coordl + j][coordc] = 1;
+                if (campo[coordl][coordc + j] === 0 && coordl + j <= 10) {
+                    campo[coordl][coordc + j] = 1;
                     k++;
                 }
+
             }
 
         }
@@ -106,7 +93,7 @@ function App() {
         return (
             <>
                 <p>
-                    {linha.map(item => item + " ")}
+                    {linha.map(item => "| " + item + " | ")}
                 </p>
 
             </>
