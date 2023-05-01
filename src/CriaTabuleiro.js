208cce4d7d2cafde57d21c053e6a25bc56cbf327
DaviPierre <zafirosafira@gmail.com>
2023-04-30 23:25:56 -0300

    Codigo funconal com as funcoes comentadas

---
+++ src/CriaTabuleiro.js
@@ -0,0 +1,34 @@
+// import geraBarco from './GeraBarco';
+// import posicionaBarco from './PosicionaBarco';
+
+// function criaTabuleiro() {
+//     const size = 10;
+//     const tam = new Array(10);
+//     var campo = new Array(size);
+//     var i = 0;
+
+//     tam[0] = 1;
+//     tam[1] = 1;
+//     tam[2] = 2;
+//     tam[3] = 2;
+//     tam[4] = 3;
+//     tam[5] = 3;
+//     tam[6] = 4;
+//     tam[7] = 4;
+//     tam[8] = 5;
+//     tam[9] = 5;
+
+
+//     for (i = 0; i < campo.length; i++) {
+//         campo[i] = new Array(10).fill(0);
+//     }
+
+//     // geraBarco();
+
+//     // posicionaBarco();
+
+//     return campo;
+
+// }
+
+// export default criaTabuleiro;
